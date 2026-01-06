b4681ded5a487889382e3d8e972df13919f9dc1a
Aleksei Shugaev <alek@Alekseis-MacBook-Pro.local>
2026-01-06 17:42:24 +0000

    magic

--- src/filters-x/Filter.ts
+++ src/filters-x/Filter.ts
@@ -1,5 +1,12 @@
+import { MOBX_ORM_FILTER_BRAND } from '../filters/Filter'
+
 export abstract class XFilter {
     abstract get URLSearchParams() : URLSearchParams
     abstract isMatch(obj: any) : boolean
     abstract get isReady() : boolean
+    
+    /**
+     * Type brand to distinguish from api filters
+     */
+    readonly __filterBrand: typeof MOBX_ORM_FILTER_BRAND = MOBX_ORM_FILTER_BRAND
 }
--- src/filters/Filter.ts
+++ src/filters/Filter.ts
@@ -1,5 +1,12 @@
+export const MOBX_ORM_FILTER_BRAND = 'mobx-orm-filter' as const
+
 export abstract class Filter {
     abstract get URLSearchParams() : URLSearchParams
     abstract setFromURI(uri: string) : void
     abstract isMatch(obj: any) : boolean
+    
+    /**
+     * Type brand to distinguish from api filters
+     */
+    readonly __filterBrand: typeof MOBX_ORM_FILTER_BRAND = MOBX_ORM_FILTER_BRAND
 }
